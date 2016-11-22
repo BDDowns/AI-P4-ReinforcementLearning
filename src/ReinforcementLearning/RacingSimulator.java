a888a519e1504d6cefd954e5a5e0bdc2c09e5aca
Bryan Downs <xerobasix@gmail.com>
2016-11-22 16:02:15 -0700

    added basic test code to RacingSimulator

--- src/ReinforcementLearning/RacingSimulator.java
+++ src/ReinforcementLearning/RacingSimulator.java
@@ -1,5 +1,7 @@
 package ReinforcementLearning;
 
+import java.io.FileReader;
+
 /**
  *  
  * @author Group 22
@@ -8,25 +10,33 @@ public class RacingSimulator {
     
     public static void main (String[] args) {
         
+        // nothing fancy yet, just make some tracks and try one at a time
+        Track l_track = new Track("L-track.txt");
+        Track o_track = new Track("O-track.txt");
+        Track r_track = new Track("R-track.txt");
+        
         /*
             To start a new race:
                 1. Instantiate a new car subclass (ex. Racecar)
                 2. Create a track from a file
                 3. Run <car>.carRace(<track>)
         */
-        
+        Car racecar = new Racecar(0,0);
+        racecar.carRace(l_track);
         
         /*
             To switch Race type
-                1. Set <car>.raceBehavior(<RaceBehavior>)
+                1. Set <car>.setRaceBehavior(<RaceBehavior>)
                 2. Run <car>.carRace(<track>)
         */
-        
-        
+        racecar.setRaceBehavior(new QLearning());
+        racecar.carRace(l_track);
         /*
             To change Crash type
-                1. Set <car>.crashBehavior(<CrashBehavior>)
+                1. Set <car>.setCrashBehavior(<CrashBehavior>)
                 2. Run <car>.carRace(<track>)
         */
+        racecar.setCrashBehavior(new BackToBeginning());
+        racecar.carRace(l_track);
     }
 }
--- src/ReinforcementLearning/Track.java
+++ src/ReinforcementLearning/Track.java
@@ -1,9 +1,21 @@
 package ReinforcementLearning;
 
+
+
 /**
  *
  * @author Group 22
  */
 public class Track {
+    private char[][] track;
     
+    
+    public Track (String file) {
+        
+    }
+    
+    
+    public char[][] getTrack() {
+        return track;
+    }
 }
