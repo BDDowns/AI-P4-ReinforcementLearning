777e6e1ad6ef9f111ab3716df75c426430b69cad
Bryan Downs <xerobasix@gmail.com>
2016-11-23 16:35:34 -0700

    added BackToBeginning logic

--- src/ReinforcementLearning/BackToBeginning.java
+++ src/ReinforcementLearning/BackToBeginning.java
@@ -8,5 +8,11 @@ public class BackToBeginning implements CrashBehavior {
     
     public void crash(Car c) {
         
+        // reset the position of the car to the starting position
+        c.xt = c.x0;
+        c.yt = c.y0;
+        // set speed of the car to 0
+        c.x_speed = 0;
+        c.y_speed = 0;
     }
 }
