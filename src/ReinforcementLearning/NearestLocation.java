f1d0361ee5114047a48fb544af228bfd92c1b3c9
Bryan Downs <xerobasix@gmail.com>
2016-11-24 08:39:41 -0700

    added second crash handling method, needs clarification from shep

--- src/ReinforcementLearning/NearestLocation.java
+++ src/ReinforcementLearning/NearestLocation.java
@@ -9,6 +9,17 @@ public class NearestLocation implements CrashBehavior {
     public void crash(Car c) {
         
         // search for nearest point of clear track
+        // assuming car is still at the crash position
+        
+        // < code to go here if above met >
+        
+        // if we don't have to search exactly as described in the problem 
+        // statement we could just subract the last known velocity vector
+        // and set speed to 0 should put us back on the track
+        c.xt -= c.x_speed;
+        c.yt -= c.y_speed;
+        c.x_speed = 0;
+        c.y_speed = 0;
         
     }
 }
