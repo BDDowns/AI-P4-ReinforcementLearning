126cf0630007489f6a325debb83e2f8ecb2f37b8
Bryan <xerobasix@gmail.com>
2016-12-11 14:28:03 -0700

    add Race constructor

--- src/ReinforcementLearning/Race.java
+++ src/ReinforcementLearning/Race.java
@@ -5,5 +5,17 @@ package ReinforcementLearning;
  * @author group22
  */
 public class Race {
+    private Integer[][][] policy;
+    private Car car;
+    private Track track;
     
+    public Race (Integer[][][] policy, Car car, Track track) {
+        this.policy = policy;
+        this.car = car;
+        this.track = track;
+    }
+    
+    public void start() {
+        
+    }
 }
